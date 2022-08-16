ad1a1f71c2ee7d90ef3a58a867048548e077c4c5
Maxim Man <y1001777@yandex.ru>
2022-08-16 11:24:44 +0300

    DZ2

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -1,7 +1,23 @@
-﻿string str1 = "456->5";
-string str2 = "782->8";
-string str3 = "918->1";
-Console.WriteLine(str1[1]);
-Console.WriteLine(str2[1]);
-Console.WriteLine(str3[1]);
+﻿// string str1 = "456->5";
+// string str2 = "782->8";
+// string str3 = "918->1";
+// Console.WriteLine(str1[1]);
+// Console.WriteLine(str2[1]);
+// Console.WriteLine(str3[1]);
+// :)
+
+
+
+// int n = int.Parse(Console.ReadLine());
+// int k = (int)Math.Log10(n)-2;
+// Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+// Console.ReadLine();
+
+
+// var day:integer;
+// begin
+// readln(day);
+// if (day >= 1) and(day < 6) then write('Рабочий')
+// else if (day > 0) and(day <= 7) then write('Выходной')
+// end.
 
